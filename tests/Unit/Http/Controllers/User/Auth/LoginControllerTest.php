c67a742104cf652d3c167c39518c7f884041553b
thanhdat <thanhdat02112001pt@gmail.com>
2022-04-06 16:43:14 +0700

    solve issue11 unit test

---
+++ tests/Unit/Http/Controllers/User/Auth/LoginControllerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers\User\Auth;
+
+use App\Models\User;
+use Tests\TestCase;
+use Faker\Factory;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Session;
+use Hash;
+
+class LoginControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testLoginUrl()
+    {
+        $response = $this->get('/login');
+        $response->assertOk();
+    }
+
+    public function testLogin()
+    {
+        Session::start();
+        $user = User::first();
+        $response = $this->post(
+            '/login',
+            [
+                '_token' => csrf_token(),
+                'email' => $user->email,
+                'password' => "02112001",
+            ]
+        );
+        $response->assertRedirect('/home');
+    }
+
+    public function testLoginFail()
+    {
+        Session::start();
+        $user = User::first();
+        $response = $this->post(
+            '/login',
+            [
+                '_token' => csrf_token(),
+                'email' => $user->email,
+                'password' => "12345",
+            ]
+        );
+        $response->assertRedirect('/login');
+    }
+
+    public function testLoginWithEmptyProfile()
+    {
+        Session::start();
+        $faker = Factory::create();
+        $formValue =
+        [
+            'email' => $faker->email,
+            'password' => Hash::make(1234),
+        ];
+        $user = User::create($formValue);
+        $response = $this->post(
+            '/login',
+            [
+                '_token' => csrf_token(),
+                'email' => User::orderBy('id', 'DESC')->first()->email,
+                'password' => 1234,
+            ]
+        );
+        $response->assertRedirect('/profile/create/'.$user->id);
+    }
+}
